1455762097781ecaeed518f7b2382d3d0793907e
quinnschwartz <quinnschwartz@gmail.com>
2016-10-21 15:53:55 -0700

    add more variables and corresponding if else statements to the js file to make the survey more accurate.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,27 @@
 
 $(document).ready(function() {
   $("form").submit(function(event) {
+    var name = $("#name").val();
+    var computers = $("#computers").val();
     var mac = $("#macorpc").val();
+    var attend = $("#attend").val();
     var company = $("#company").val();
     var describe = $("input:radio[name=describe]:checked").val();
 
 
     var result = function(){
 
-      if (mac === "Mac" && company === "SQ1" && describe === "artistic") {
+      if (computers === "no") {
+        return ("If you don't like working with computers then maybe you should reconsider studying at Epicodus!");
+      } else if (describe === "artistic") {
         return ("Design!");
-      } else if (mac === "Mac" && company === "SQ1" && describe === "logical") {
+      } else if (describe === "logical" && attend === "January") {
           return ("PHP!");
-      } else if (mac === "PC" && company === "Microsoft" && describe === "logical") {
+      } else if (mac === "PC" && describe === "logical") {
           return ("C#!");
-      } else if (mac === "Mac" && company === "Twitter" && describe === "logical") {
+      } else if (mac === "PC" && describe === "logical") {
+          return ("C#!");
+      } else if (mac === "Mac" && describe === "logical") {
           return ("Ruby!");
       }
     };
