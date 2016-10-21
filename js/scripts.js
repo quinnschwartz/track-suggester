ada5532fe61746d6f07ca610c56d6bad7a896a28
quinnschwartz <quinnschwartz@gmail.com>
2016-10-21 15:06:11 -0700

    fix bugs in the JS file, remove typos so that the code can run through.

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,28 @@
-//User Logic
-$(document).ready(function()) {
-  $("form#questions").submit(function(event) {
 
-    var name = $("input#name").val();
-    var computers = $("input#computers").val();
-    var attend = $("select#attend").val();
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    var mac = $("#macorpc").val();
+    var company = $("#company").val();
     var describe = $("input:radio[name=describe]:checked").val();
-    var company = $("select#company").val();
-    var mac = $("input#macorpc").val();
 
 
+    var result = function(){
 
-    $("#output").text(result);
-    $("#trackSuggestion").show();
-event.preventDefault();
-  });
-});
+      if (mac === "Mac" && company === "SQ1" && describe === "Artistic") {
+        return ("Design!");
+      } else if (mac === "Mac" && company === "SQ1" && describe === "Logical") {
+          return ("PHP!");
+      } else if (mac === "PC" && company === "Microsoft" && describe === "Logical") {
+          return ("C#!");
+      } else if (mac === "Mac" && company === "Twitter" && describe === "Logical") {
+          return ("Ruby!");
+      }
+    };
 
-//Business Logic
 
-var result = function(){
+        $("#output").text(result);
+        $("#track").show();
 
-  if ( computers === "no") {
-    return ("If you don't like computers then maybe you should reconsider attending Epicodus...");
-  } else if (describe === "Artistic") {
-    return ("Design!");
-  } else if (attend === "October" && describe === "Logical" && company === "Twitter") {
-    return ("Ruby!");
-  } else if (attend === "January" && company === "SQ1") {
-    return ("PHP!");
-  } else if (attend === "October" && describe === "Logical" && mac = PC) {
-    return ("C#!");
-};
+        event.preventDefault();
+        });
+      });
