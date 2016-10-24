b87cd9017c3baac0686e6e7889a51a8dc48a7868
quinnschwartz <quinnschwartz@gmail.com>
2016-10-23 21:29:19 -0700

    separate out user front end and back end logic in js code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,37 @@
+//Business Logic
 
-$(document).ready(function() {
-  $("form").submit(function(event) {
-    var name = $("#name").val();
-    var computers = $("#computers").val();
-    var mac = $("#macorpc").val();
-    var attend = $("#attend").val();
-    var company = $("#company").val();
-    var describe = $("input:radio[name=describe]:checked").val();
+var trackSuggestion = function(computers, mac, attend, describe){
 
-    var result = function(){
+  if (computers === "no") {
+    return ("If you don't like working with computers then maybe you should reconsider studying at Epicodus!");
+  } else if (describe === "artistic") {
+    return ("CSS/Design!");
+  } else if (describe === "logical" && attend === "January") {
+      return ("PHP/Drupal!");
+  } else if (mac === "PC" && describe === "logical") {
+      return ("C#/.NET!");
+  } else if (mac === "Mac" && describe === "logical") {
+      return ("Ruby/Rails!");
+  }
+};
 
-      if (computers === "no") {
-        return ("If you don't like working with computers then maybe you should reconsider studying at Epicodus!");
-      } else if (describe === "artistic") {
-        return ("CSS/Design!");
-      } else if (describe === "logical" && attend === "January") {
-          return ("PHP/Drupal!");
-      } else if (mac === "PC" && describe === "logical") {
-          return ("C#/.NET!");
-      } else if (mac === "Mac" && describe === "logical") {
-          return ("Ruby/Rails!");
-      }
-    };
+//User Interface Logic
+
+    $(document).ready(function() {
+      $("form").submit(function(event) {
+        event.preventDefault();
+        var name = $("#name").val();
+        var computers = $("#computers").val();
+        var mac = $("#macorpc").val();
+        var attend = $("#attend").val();
+        var company = $("#company").val();
+        var describe = $("input:radio[name=describe]:checked").val();
+        var result = trackSuggestion(computers, mac, attend, describe);
 
     $(".name").text(name);
     $("#output").text(result);
     $("#track").show();
 
-  event.preventDefault();
+
   });
 });
